cceec36d84e459c37b22dc241eb416eec2381a43
Dong Pham <dong.orc12@gmail.com>
2025-01-25 20:26:05 +0700

    3.1 Init angular project with core ui

---
+++ src/DuAnBlog.Api/SwaggerNullableParameterFilter.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper.Internal;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace DuAnBlog.Api;
+
+public class SwaggerNullableParameterFilter : IParameterFilter
+{
+    public void Apply(OpenApiParameter parameter, ParameterFilterContext context)
+    {
+        parameter.Schema.Nullable = true;
+        if (!parameter.Schema.Nullable &&
+            (context.ApiParameterDescription.Type.IsNullableType() || !context.ApiParameterDescription.Type.IsValueType))
+        {
+        }
+    }
+}
