04d3ceb2f49c023e8eaaac7a49c2d7c402a720ca
Dong Pham <dong.orc12@gmail.com>
2025-01-28 15:26:45 +0700

    3.9. Logout and loading

--- duan-blog-admin-ui/src/app/containers/default-layout/default-header/default-header.component.ts
+++ duan-blog-admin-ui/src/app/containers/default-layout/default-header/default-header.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-
+import { Router } from '@angular/router';
+import { TokenStorageService } from '../../../shared/services/token.storage.service';
+import { UrlConstants } from '../../../shared/constants/url.constant';
 import { ClassToggleService, HeaderComponent } from '@coreui/angular';
 
 @Component({
@@ -8,14 +10,21 @@ import { ClassToggleService, HeaderComponent } from '@coreui/angular';
   templateUrl: './default-header.component.html',
 })
 export class DefaultHeaderComponent extends HeaderComponent {
+  @Input() sidebarId: string = 'sidebar';
 
-  @Input() sidebarId: string = "sidebar";
+  public newMessages = new Array(4);
+  public newTasks = new Array(5);
+  public newNotifications = new Array(5);
 
-  public newMessages = new Array(4)
-  public newTasks = new Array(5)
-  public newNotifications = new Array(5)
-
-  constructor(private classToggler: ClassToggleService) {
+  constructor(
+    private classToggler: ClassToggleService,
+    private tokenService: TokenStorageService,
+    private router: Router
+  ) {
     super();
+  }
+  logout() {
+    this.tokenService.signOut();
+    this.router.navigate([UrlConstants.LOGIN]);
   }
 }
--- duan-blog-admin-ui/src/app/views/auth/login/login.component.ts
+++ duan-blog-admin-ui/src/app/views/auth/login/login.component.ts
@@ -1,22 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import {
   LoginRequest,
   AdminApiAuthApiClient,
   AuthenticatedResult,
 } from '../../../api/admin-api.service.generated';
-import { AlertService } from 'src/app/shared/services/alert.service';
-import { UrlConstants } from 'src/app/shared/constants/url.constant';
+import { AlertService } from '../../../shared/services/alert.service';
+import { UrlConstants } from '../../../shared/constants/url.constant';
 import { Router } from '@angular/router';
-import { TokenStorageService } from 'src/app/shared/services/token.storage.service';
+import { TokenStorageService } from '../../../shared/services/token.storage.service';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnDestroy {
   loginForm: FormGroup;
+  private ngUnsubscribe: Subject<void> = new Subject<void>();
+  loading = false;
   constructor(
     private fb: FormBuilder,
     private authClient: AdminApiAuthApiClient,
@@ -29,24 +32,34 @@ export class LoginComponent {
       password: ['', [Validators.required]],
     });
   }
+  ngOnDestroy(): void {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+  }
 
   onLogin() {
+    this.loading = true;
     var loginRequest: LoginRequest = new LoginRequest({
       username: this.loginForm.controls['username'].value,
       password: this.loginForm.controls['password'].value,
     });
-    this.authClient.login(loginRequest).subscribe({
-      next: (response: AuthenticatedResult) => {
-        // Save token and refresh token info to local storage
-        this.tokenStorageService.saveToken(response.token);
-        this.tokenStorageService.saveRefreshToken(response.refreshToken);
-        this.tokenStorageService.saveUser(response);
-        this.router.navigate([UrlConstants.HOME]);
-      },
-      error: (error: any) => {
-        console.error(error);
-        this.alertService.showError('Đăng nhập thất bại');
-      },
-    });
+    this.authClient
+      .login(loginRequest)
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe({
+        next: (response: AuthenticatedResult) => {
+          // Save token and refresh token info to local storage
+          this.tokenStorageService.saveToken(response.token);
+          this.tokenStorageService.saveRefreshToken(response.refreshToken);
+          this.tokenStorageService.saveUser(response);
+          this.loading = false;
+          this.router.navigate([UrlConstants.HOME]);
+        },
+        error: (error: any) => {
+          this.loading = false;
+          console.error(error);
+          this.alertService.showError('Đăng nhập thất bại');
+        },
+      });
   }
 }
